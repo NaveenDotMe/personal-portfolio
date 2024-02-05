9f62c4ac06598834d63eff3dd73d6dbf6b313d31
NaveenDotMe <elmatadordelmar99@gmail.com>
2024-02-05 22:16:20 +0530

    add new portfolio images

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ====================== Typing Animation ====================== */
 var typed = new Typed(".typing", {
-    strings:["Java Developer","Web Developer","Content Writter"],
+    strings:["Java Developer","Web Developer","Content Writer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
