e5bfc6fdcb323686a26e15266ff3db3c56ea7ff0
NaveenDotMe <elmatadordelmar99@gmail.com>
2024-02-05 21:57:09 +0530

    add typing animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 /* ====================== Typing Animation ====================== */
+var typed = new Typed(".typing", {
+    strings:["Java Developer","Web Developer","Content Writter"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
