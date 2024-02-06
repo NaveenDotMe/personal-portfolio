0196b735aeef2624374646ef110a22674d45c1e8
NaveenDotMe <elmatadordelmar99@gmail.com>
2024-02-06 21:17:52 +0530

    Add Section aside animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 /* ====================== Typing Animation ====================== */
 var typed = new Typed(".typing", {
-    strings:["Java Developer","Web Developer","Content Writer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
+    strings: ["Java Developer", "Web Developer", "Content Writer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
 })
+
+/* ====================== Aside ====================== */
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+for (let i = 0; i < totalNavList; i++) 
+    {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () 
+    {
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("back-section");
+        }
+        for (let j = 0; j < totalNavList; j++) 
+        {
+            if(navList[j].querySelector("a").classList.contains("active"))
+            {
+                allSection[j].classList.add("back-section");
+            }
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+}
+function showSection(elements) 
+{
+    for(let i=0; i<totalSection; i++){
+        allSection[i].classList.remove("active");
+    }
+    const target = elements.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
